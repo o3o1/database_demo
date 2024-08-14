fae562d709ee02fd1201d4078fd5dd49e5be77ef
choco <z.0301@outlook.com>
2024-08-14 23:44:01 +0800

    Add hint

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             dgStudent = new DataGridView();
-            Column1 = new DataGridViewTextBoxColumn();
-            Column2 = new DataGridViewTextBoxColumn();
-            Column3 = new DataGridViewTextBoxColumn();
-            Column4 = new DataGridViewTextBoxColumn();
             panel1 = new Panel();
             btnModify = new Button();
             btnSearch = new Button();
             btnDelete = new Button();
             btnAdd = new Button();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)dgStudent).BeginInit();
             panel1.SuspendLayout();
             SuspendLayout();
@@ -53,30 +53,6 @@
             dgStudent.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dgStudent.Size = new Size(776, 333);
             dgStudent.TabIndex = 0;
-            // 
-            // Column1
-            // 
-            Column1.DataPropertyName = "ID";
-            Column1.HeaderText = "学号";
-            Column1.Name = "Column1";
-            // 
-            // Column2
-            // 
-            Column2.DataPropertyName = "StudentName";
-            Column2.HeaderText = "姓名";
-            Column2.Name = "Column2";
-            // 
-            // Column3
-            // 
-            Column3.DataPropertyName = "Age";
-            Column3.HeaderText = "年龄";
-            Column3.Name = "Column3";
-            // 
-            // Column4
-            // 
-            Column4.DataPropertyName = "ClassID";
-            Column4.HeaderText = "班级";
-            Column4.Name = "Column4";
             // 
             // panel1
             // 
@@ -133,6 +109,34 @@
             btnAdd.Text = "增加";
             btnAdd.UseVisualStyleBackColor = true;
             btnAdd.Click += btnAdd_Click;
+            // 
+            // Column1
+            // 
+            Column1.DataPropertyName = "ID";
+            Column1.HeaderText = "学号（数字）";
+            Column1.Name = "Column1";
+            Column1.ToolTipText = "数字";
+            // 
+            // Column2
+            // 
+            Column2.DataPropertyName = "StudentName";
+            Column2.HeaderText = "姓名（字符串）";
+            Column2.Name = "Column2";
+            Column2.ToolTipText = "字符串";
+            // 
+            // Column3
+            // 
+            Column3.DataPropertyName = "Age";
+            Column3.HeaderText = "年龄（数字）";
+            Column3.Name = "Column3";
+            Column3.ToolTipText = "数字";
+            // 
+            // Column4
+            // 
+            Column4.DataPropertyName = "ClassID";
+            Column4.HeaderText = "班级（数字）";
+            Column4.Name = "Column4";
+            Column4.ToolTipText = "数字";
             // 
             // MainForm
             // 
